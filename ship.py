12e3dc2d62c1ea1006b991a6177738048503dbca
minitower <minitower.rus@gmail.com>
2020-10-12 23:48:14 +0300

    Sprite for ships

--- ship.py
+++ ship.py
@@ -1,17 +1,20 @@
 import pygame
 import os
+from pygame.sprite import Sprite
 
 current_path = os.path.dirname (__file__)
 resource_path = os.path.join (current_path, 'res')
+
+image = pygame.image.load (os.path.join (resource_path, 'rocket-2442125.png'))
+scale = pygame.transform.scale(image, (image.get_width()//9, image.get_height()//9))
 
 class Ship():
 
     def __init__(self, ai_setings, screen):
         self.screen = screen
         self.ai_setings = ai_setings
-        self.image = pygame.image.load (os.path.join (resource_path, 'rocket-2442125.png'))
-        self.scale = pygame.transform.scale(self.image, (self.image.get_width()//9, self.image.get_height()//9))
-        self.rect = self.scale.get_rect()
+        self.image = scale
+        self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
@@ -35,7 +38,7 @@ class Ship():
         self.rect.centerx = self.center
 
     def blitme (self):
-        self.screen.blit (self.scale, self.rect)
+        self.screen.blit (self.image, self.rect)
 
     def center_ship (self):
         self.rect.centerx = self.screen_rect.centerx
