64d58cae52f75d2f96a5b601332fdc86e0e0b917
minitower <minitower.rus@gmail.com>
2020-10-12 23:51:56 +0300

    Score and highscore

---
+++ scoreboard.py
@@ -0,0 +1,68 @@
+import pygame
+from pygame.sprite import Group
+from lives import Lives
+import os
+
+
+current_path = os.path.dirname (__file__)
+resource_path = os.path.join (current_path, 'res')
+
+image = pygame.image.load (os.path.join (resource_path, 'rocket-2442125.png'))
+
+class scoreboard():
+
+    def __init__(self, ai_setings, screen, stats):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_setings = ai_setings
+        self.stats = stats
+
+        self.text_color = (30,30,30)
+        self.font = pygame.font.SysFont (None, 48)
+        self.perp_score()
+        self.perp_high_score ()
+        self.perp_level ()
+        self.perp_ship()
+
+    def perp_score (self):
+        rounded_score = int (round(self.stats.score, -1))
+        score_str = '{:,}'.format (rounded_score)
+        self.score_image = self.font.render (score_str, True, self.text_color,
+                                            self.ai_setings.bg_color)
+
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def perp_high_score (self):
+        high_score = int (round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+
+        self.high_score_image = self.font.render (high_score_str, True, self.text_color,
+        self.ai_setings.bg_color)
+
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top
+
+    def perp_level (self):
+        self.level_image =self.font.render (str(self.stats.level),
+        True, self.text_color, self.ai_setings.bg_color)
+
+        self.level_rect = self.level_image.get_rect ()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def perp_ship (self):
+        self.ships = Group ()
+        for ship_number in range (self.stats.ships_left):
+            ship = Lives (self.ai_setings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add (ship)
+
+    def show_score(self):
+        self.screen.blit (self.score_image, self.score_rect)
+        self.screen.blit (self.high_score_image, self.high_score_rect)
+        self.screen.blit (self.level_image, self.level_rect)
+        self.ships.draw (self.screen)
