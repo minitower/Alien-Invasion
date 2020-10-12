67293b0c2b892eca54a1d9cfdf0b923cd5b9be96
minitower <minitower.rus@gmail.com>
2020-10-12 23:45:45 +0300

    Button Play

--- button.py
+++ button.py
@@ -5,7 +5,7 @@ class Button ():
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.width, self.height = 300, 100
-        self.button_color = (255, 255, 0)
+        self.button_color = (0, 255, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
         self.rect = pygame.Rect(0, 0, self.width, self.height)
