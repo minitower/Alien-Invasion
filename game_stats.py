766135f4d036e4e325d8bad346543e9e97f91658
minitower <minitower.rus@gmail.com>
2020-10-12 23:47:41 +0300

    Stats

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,9 @@ class GameStats ():
         self.ai_setings = ai_setings
         self.reset_stats()
         self.game_active = False
+        self.high_score = 0
 
     def reset_stats (self):
         self.ships_left = self.ai_setings.ship_limit
+        self.score = 0
+        self.level = 1
